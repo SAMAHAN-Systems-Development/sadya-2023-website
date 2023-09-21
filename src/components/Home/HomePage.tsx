89a3fa87e7e3ddb63ba9d7b6d282f651f99d9323
unknown <paololuisramirez@gmail.com>
2023-09-21 13:45:12 +0800

    feat: created NavigationBar (4)

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Button from '@/components/ui/Button';
+import NavigationBar from '../ui/NavigationBar';
 
 const HomePage = () => {
   return (
@@ -8,6 +9,7 @@ const HomePage = () => {
       <Button label="Primary Button" type="primary" />
       <Button label="Secondary Button" type="secondary" />
       <Button label="Large Button" size="large" />
+      <NavigationBar/>
     </div>
   );
 };
