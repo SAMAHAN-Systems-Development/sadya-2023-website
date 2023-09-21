f05227104ff96c1e0efaaca798b6f2b357c268f0
Mariel <marmabs692@gmail.com>
2023-09-21 12:14:07 +0800

    feat: created developers data (#19)

---
+++ src/data/developers.ts
@@ -0,0 +1,83 @@
+import { OfficerType } from "../../lib/types/officerType";
+import picSample from "../../public/assets/images/SysEgg.png"
+
+export const Developers: OfficerType[] = [
+    {
+        imageUrl: picSample,
+        firstName: "Angelli Kezzel",
+        lastName: "Alkuino",
+        department: "Front-End Developer",
+    },
+    {
+        imageUrl: picSample,
+        firstName: "Louis Miguel",
+        lastName: "Pawaon",
+        department: "Front-End Developer",
+    },
+    {
+        imageUrl: picSample,
+        firstName: "Miguel Emmanuel",
+        lastName: "Dailisan",
+        department: "Front-End Developer",
+    },
+    {
+        imageUrl: picSample,
+        firstName: "Jiyo Seth",
+        lastName: "Valmoria",
+        department: "Full-Stack Developer",
+    },
+    {
+        imageUrl: picSample,
+        firstName: "Mariel",
+        lastName: "Mabano",
+        department: "Front-End Developer",
+    },
+    {
+        imageUrl: picSample,
+        firstName: "Paolo Luis",
+        lastName: "Ramirez",
+        department: "Front-End Developer",
+    },
+    {
+        imageUrl: picSample,
+        firstName: "Mary Rose Jofel",
+        lastName: "Villacampa",
+        department: "Front-End Developer",
+    },
+    {
+        imageUrl: picSample,
+        firstName: "John Kyle",
+        lastName: "Lampa",
+        department: "Front-End Developer",
+    },
+    {
+        imageUrl: picSample,
+        firstName: "Viver Val",
+        lastName: "Bungag",
+        department: "Back-End Developer",
+    },
+    {
+        imageUrl: picSample,
+        firstName: "Marvin Paul",
+        lastName: "Eloja",
+        department: "Back-End Developer",
+    },
+    {
+        imageUrl: picSample,
+        firstName: "Francis Rhaey",
+        lastName: "Casas",
+        department: "Back-End Developer",
+    },
+    {
+        imageUrl: picSample,
+        firstName: "Jiyo Seth",
+        lastName: "Valmoria",
+        department: "Back-End Developer",
+    },
+    {
+        imageUrl: picSample,
+        firstName: "Maverick Jasper",
+        lastName: "Pigao",
+        department: "UI/UX Designer",
+    },
+];
