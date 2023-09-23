004c07ccae84964e800ff11df327bf5d3469de5a
Sunny <maryrosedvillacampa@gmail.com>
2023-09-23 14:49:10 +0800

    feat: created EventData #18

---
+++ lib/types/EventDataTypes.ts
@@ -0,0 +1,9 @@
+import type { StaticImageData } from 'next/image';
+
+export type EventDataTypes = {
+  imageUrl: StaticImageData;
+  eventDataEntry: string;
+  eventDataDate: string;
+  eventDataDescription: string;
+  eventDataFloor: string;
+};
---
+++ src/data/EventData.ts
@@ -0,0 +1,19 @@
+import type { EventDataTypes } from "../../lib/types/EventDataTypes";
+
+export const EventData: EventDataTypes[] = [
+    {
+        eventDataEntry: 'TIKCETED ENTRY',
+        eventDataDate: 'October 23, 2023',
+        eventDataDescription: 'Contest smthn??',
+        eventDataFloor: 'Martin Hall - 4th Floor - Lorem Ipsum',
+    },
+];
+
+export const BTOTB: EventDataTypes[] = [
+    {
+        eventDataEntry: 'TIKCETED ENTRY',
+        eventDataDate: 'October 23, 2023',
+        eventDataDescription: 'BTOB data here',
+        eventDataFloor: 'Martin Hall - 4th Floor - Lorem Ipsum',
+    },
+];
