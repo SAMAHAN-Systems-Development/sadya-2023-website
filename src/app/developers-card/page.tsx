a915d65e16a266af4c875b5e1ca60e38f6470407
Mariel <marmabs692@gmail.com>
2023-09-25 18:32:22 +0800

    feat: developer card component

---
+++ src/app/developers-card/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DeveloperSection from '@/components/ui/DeveloperSection';
+
+function page() {
+  return (
+    <div>
+      <DeveloperSection />
+    </div>
+  );
+}
+
+export default page;
