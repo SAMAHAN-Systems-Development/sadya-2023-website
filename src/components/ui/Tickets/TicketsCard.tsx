106c0d64c7a94c23ce65988e33c7af0175584de6
Miraii133 <jiyovalmoria@gmail.com>
2023-09-26 22:29:47 +0800

    feat: created initial TicketsCard (#16)

--- src/components/ui/Tickets/TicketsCard.tsx
+++ src/components/ui/Tickets/TicketsCard.tsx
@@ -1,29 +1,9 @@
 import React from 'react';
 
 const TicketsCard = () => {
-  return (
-<div className="w-1/2 flex flex-col mx-5">
-    <div className="p-4 my-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-      <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">5F</h5>
-      <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">56 Tickets Left</p>
-    </div>
-  
-  <div className="p-4 my-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-      <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">5F</h5>
-      <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">56 Tickets Left</p>
-      </div>
-    <div className="p-4 my-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-      <h5 className="mb -2 text-3xl font-bold text-gray-900 dark:text-white">5F</h5>
-      <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">56 Tickets Left</p>
-         </div>
-<div className="p-4 my-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-      <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">5F</h5>
-      <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">56 Tickets Left</p>
-      </div>
  
-
-
-</div>
+  return (
+    <></>
   );
 };
 
