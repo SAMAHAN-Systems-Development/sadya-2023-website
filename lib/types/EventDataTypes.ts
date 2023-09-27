1874821916394e0adaa9c4ef9766a179fd65c73f
iyabc <juliennepanes@gmail.com>
2023-09-27 21:18:10 +0800

    refactor: event type made imageurl optional

--- lib/types/EventDataTypes.ts
+++ lib/types/EventDataTypes.ts
@@ -1,9 +1,9 @@
 import type { StaticImageData } from 'next/image';
 
 export type EventDataTypes = {
-  imageUrl: StaticImageData;
-  eventDataEntry: string;
   eventDataDate: string;
   eventDataDescription: string;
+  eventDataEntry: string;
   eventDataFloor: string;
+  imageUrl?: StaticImageData;
 };
