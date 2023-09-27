d2e2642576fec1f6c3cc83d75f44102c242eaadb
iyabc <juliennepanes@gmail.com>
2023-09-27 21:22:47 +0800

    refactor: fixed eslint errors

--- src/components/ui/EventCard.tsx
+++ src/components/ui/EventCard.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import Head from 'next/head';
 
 interface CardProps {
-  title: string;
   date: string;
   description: string;
   floor: string;
+  title: string;
 }
 
-const EventCard: React.FC<CardProps> = ({ title, date, description, floor }) => {
+const EventCard: React.FC<CardProps> = ({
+  title,
+  date,
+  description,
+  floor,
+}) => {
   return (
     <div className="flex justify-center items-center h-screen">
-        <div className="max-w-sm rounded-xl overflow-hidden shadow-lg bg-black">
-            <div className="px-4 py-4">
-                <div className="text-base mb-2">{title}</div>
-                <p className="text-gray-700 text-base mt-20">{date}</p>
-                <p className="font-bold text-gray-700 text-xl">{description}</p>
-                <p className="text-gray-700 text-base">{floor}</p>
-            </div>
+      <div className="max-w-sm rounded-xl overflow-hidden shadow-lg bg-black">
+        <div className="px-4 py-4">
+          <div className="text-base mb-2">{title}</div>
+          <p className="text-gray-700 text-base mt-20">{date}</p>
+          <p className="font-bold text-gray-700 text-xl">{description}</p>
+          <p className="text-gray-700 text-base">{floor}</p>
         </div>
+      </div>
     </div>
   );
 };
--- src/stories/ui/EventCard.stories.tsx
+++ src/stories/ui/EventCard.stories.tsx
@@ -1,21 +1,21 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from '@storybook/react';
 
-import EventCard from "../../components/ui/EventCard";
+import EventCard from 'src/components/ui/EventCard';
 
 const meta: Meta<typeof EventCard> = {
-    title: 'Event Card',
-    component: EventCard,
-  };
-  
-  export default meta;
-  type Story = StoryObj<typeof EventCard>;
-  
-  export const Primary: Story = {
-    args: {
-      title: 'Ticketed Entry',
-      date:'October 23, 2023',
-      description: 'BTOB & Creative Series Short Film Showcase',
-      floor: 'Floor - Building - Lorem Ipsum'
-    },
-    render: (args) => <EventCard {...args}/>,
-  };
+  title: 'Event Card',
+  component: EventCard,
+};
+
+export default meta;
+type Story = StoryObj<typeof EventCard>;
+
+export const Primary: Story = {
+  args: {
+    title: 'Ticketed Entry',
+    date: 'October 23, 2023',
+    description: 'BTOB & Creative Series Short Film Showcase',
+    floor: 'Floor - Building - Lorem Ipsum',
+  },
+  render: (args) => <EventCard {...args} />,
+};
