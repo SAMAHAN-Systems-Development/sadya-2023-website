0755be1708665d67b8024e759ca32a039a22b664
Mariel <marmabs692@gmail.com>
2023-10-03 15:30:45 +0800

    refactor: changed gradient placement

--- src/components/ui/DeveloperCard.tsx
+++ src/components/ui/DeveloperCard.tsx
@@ -24,39 +24,38 @@ const DeveloperCard: React.FC<DeveloperCardProps> = ({
         className="rounded-card object-cover object-center"
         alt="/"
       />
-      <div className="bg-gradient-to-t from-[#080A3C] to-transparent w-full h-[100%]">
-        <div className="absolute bottom-0 left-0 px-5 pb-7 pl-5">
-          <span className="text-sm font-inter text-[#EFD807]">
-            {developerType}
-          </span>
-          <h1 className="text-2xl font-bold font-inter text-[#CFEF07]">
-            {fullName}
-          </h1>
-          <span className="text-sm font-inter font-light text-white">
-            {course}
-          </span>
-          <div className="flex pt-3 gap-4 text-3xl text-white">
-            <Link
-              className="hover:-translate-y-1 transition-all duration-300"
-              href={facebook}
-            >
-              <FaFacebook />
-            </Link>
-            <Link
-              className="hover:-translate-y-1 transition-all duration-300"
-              href={instagram}
-            >
-              <FaInstagram />
-            </Link>
-            <Link
-              className="hover:-translate-y-1 transition-all duration-300"
-              href={twitter}
-            >
-              <FaTwitter />
-            </Link>
-          </div>
+      <div className="absolute bottom-0 left-0 px-5 pb-7 pl-5 z-10">
+        <span className="text-sm font-inter text-[#EFD807]">
+          {developerType}
+        </span>
+        <h1 className="text-2xl font-bold font-inter text-[#CFEF07]">
+          {fullName}
+        </h1>
+        <span className="text-sm font-inter font-light text-white">
+          {course}
+        </span>
+        <div className="flex pt-3 gap-4 text-3xl text-white">
+          <Link
+            className="hover:-translate-y-1 transition-all duration-300"
+            href={facebook}
+          >
+            <FaFacebook />
+          </Link>
+          <Link
+            className="hover:-translate-y-1 transition-all duration-300"
+            href={instagram}
+          >
+            <FaInstagram />
+          </Link>
+          <Link
+            className="hover:-translate-y-1 transition-all duration-300"
+            href={twitter}
+          >
+            <FaTwitter />
+          </Link>
         </div>
       </div>
+      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-[#080A3C] to-transparent rounded-[0.9375rem]" />
     </div>
   );
 };
