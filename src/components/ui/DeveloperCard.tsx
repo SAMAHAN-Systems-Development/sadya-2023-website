4d176bfdeb54b2f85ca187202fed67b5c902e112
Migs <miguelemmanueldailisan@gmail.com>
2023-10-03 22:39:46 +0800

    chore: merge

--- src/components/ui/DeveloperCard.tsx
+++ src/components/ui/DeveloperCard.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
 import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa6';
-<<<<<<< HEAD
-import type { StaticImageData } from 'next/image';
-import Image from 'next/image';
-import Link from 'next/link';
-
-type DeveloperCardProps = {
-  course: string;
-  developerType: string;
-  facebook: string;
-  fullName: string;
-  imageUrl: StaticImageData;
-  instagram: string;
-  twitter: string;
-};
-=======
 import Image from 'next/image';
 import Link from 'next/link';
 
 import type { DeveloperCardProps } from 'lib/types/developersDatatype';
 
->>>>>>> ff82a5d44196b9a49df0dec6ac8c4e24e0a377ef
 const DeveloperCard: React.FC<DeveloperCardProps> = ({
   course,
   developerType,
@@ -40,40 +24,6 @@ const DeveloperCard: React.FC<DeveloperCardProps> = ({
         className="rounded-card object-cover object-center"
         alt="/"
       />
-<<<<<<< HEAD
-      <div className="absolute bottom-0 left-0 px-5 pb-7 pl-5 z-10">
-        <span className="text-sm font-inter text-[#EFD807]">
-          {developerType}
-        </span>
-        <h1 className="text-2xl font-bold font-inter text-[#CFEF07]">
-          {fullName}
-        </h1>
-        <span className="text-sm font-inter font-light text-white">
-          {course}
-        </span>
-        <div className="flex pt-3 gap-4 text-3xl text-white">
-          <Link
-            className="hover:-translate-y-1 transition-all duration-300"
-            href={facebook}
-          >
-            <FaFacebook />
-          </Link>
-          <Link
-            className="hover:-translate-y-1 transition-all duration-300"
-            href={instagram}
-          >
-            <FaInstagram />
-          </Link>
-          <Link
-            className="hover:-translate-y-1 transition-all duration-300"
-            href={twitter}
-          >
-            <FaTwitter />
-          </Link>
-        </div>
-      </div>
-      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-[#080A3C] to-transparent rounded-[0.9375rem]" />
-=======
       <div className="bg-gradient-to-t from-[#080A3C] to-transparent w-full h-[100%]">
         <div className="absolute bottom-0 left-0 px-5 pb-7 pl-5">
           <span className="text-sm font-inter text-[#EFD807]">
@@ -107,7 +57,6 @@ const DeveloperCard: React.FC<DeveloperCardProps> = ({
           </div>
         </div>
       </div>
->>>>>>> ff82a5d44196b9a49df0dec6ac8c4e24e0a377ef
     </div>
   );
 };
