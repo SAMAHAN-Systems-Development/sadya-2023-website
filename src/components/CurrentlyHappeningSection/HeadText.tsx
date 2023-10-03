bafff4107e0e09c20c312e15a297b7091a0754bb
Sunny <maryrosedvillacampa@gmail.com>
2023-10-04 02:40:35 +0800

    "refactor: removed old version of the code (#10)"

--- src/components/CurrentlyHappeningSection/HeadText.tsx
+++ src/components/CurrentlyHappeningSection/HeadText.tsx
@@ -1,21 +1,3 @@
-// import React from "react";
-
-// const textStyle = {
-//     fontSize: "65px",
-// }
-// export default function HeadText() {
-//     return(
-//         <main>
-//             <div className="flex flex-col justify-center items-center">
-//                 <p className="flex uppercase text-center" style={textStyle}>
-//                     currently happening</p>
-//                 <p className="text-center">
-//                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-//             </div>
-//         </main>
-//     );
-// }
-
 import React from "react";
 
 export default function HeadText() {
