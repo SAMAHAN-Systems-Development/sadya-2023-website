c4ae90a346f17e28a0dab1b289cbd05be41c984f
Migs <miguelemmanueldailisan@gmail.com>
2023-10-06 22:38:15 +0800

    refactor: fixed responsiveness

--- src/components/ui/SectionTitle.tsx
+++ src/components/ui/SectionTitle.tsx
@@ -8,11 +8,11 @@ type TextProps = {
 const SectionTitle: React.FC<TextProps> = ({ subtitle, title }) => {
   return (
     <>
-      <div className="flex flex-col sm:w-full justify-center items-center text-center bg-blue">
+      <div className="flex flex-col w-full justify-center items-center text-center">
         <h1 className="font-semibold text-crimson font-londrina 2xl:text-8xl xl:text-8xl lg:text-7xl md:text-7xl sm:text-7xl xsm:text-7xl text-5xl flex-wrap uppercase">
           {title}
         </h1>
-        <div className="lg:w-[31.188rem] xl:w-[31.188rem] md:w-[30.9] sm:w-[24rem] bg-orange">
+        <div className="lg:w-[31.188rem] xl:w-[31.188rem] md:w-[30.9] sm:w-[24rem]">
           <p className="text-xs sm:text-base font-normal font-inter flex flex-wrap text-black normal-case mt-2 text-center">
             {subtitle}
           </p>
