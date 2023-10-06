37a0f33d841c4858cfe0bf6612a9895a998c0f49
Miraii133 <jiyovalmoria@gmail.com>
2023-10-06 20:08:28 +0800

    chore: moved TicketSection from 16-Ticket_Dropdown_Button to Ticket_Section.

---
+++ src/components/TicketSection/MapTicketCard.tsx
@@ -0,0 +1,71 @@
+
+import TicketsCard from '@/components/ui/Tickets/TicketsCard'
+import {
+  firstCard, secondCard, 
+  firstCard_header, secondCard_header
+} from '@/data/ticketsdata'
+
+
+export default function MapTicketCard(){
+     //const splicedTicketData = TicketsData.splice(0, 4)
+    return (
+      <div className="h-[30rem] grid grid-cols-2 justify-center">
+        
+       
+      <div className="px-5 py-5">
+        <div className="flex flex-col text-center py-5">
+          <h1 className="font-bold">
+                {firstCard_header.eventTitle}
+            </h1>
+              <h2>
+                {firstCard_header.eventDate}
+              </h2>
+        </div>
+           
+            {firstCard.map((ticketData, index) => (
+              <TicketsCard
+              key={index}
+              eventFloor = {ticketData.eventFloor}
+              eventPrice = {ticketData.eventPrice}
+              eventFoodAvailability={ticketData.eventFoodAvailability}
+              eventBuilding = {ticketData.eventBuilding}
+              eventSlotLeft = {ticketData.eventSlotLeft}
+              eventTime = {ticketData.eventTime}
+            />
+              
+            
+          
+          ))}
+
+    </div>
+
+      <div className="px-5 py-5">
+     <div className="flex flex-col text-center py-5">
+            <h1 className="font-bold">
+                {secondCard_header.eventTitle}
+            </h1>
+              <h2>
+                {secondCard_header.eventDate}
+              </h2>
+        </div>
+           
+         
+          {secondCard.map((ticketData, index) => (
+            <TicketsCard
+              key={index}
+             
+              eventFloor = {ticketData.eventFloor}
+              eventPrice = {ticketData.eventPrice}
+              eventFoodAvailability={ticketData.eventFoodAvailability}
+              eventBuilding={ticketData.eventBuilding}
+              eventSlotLeft = {ticketData.eventSlotLeft}
+              eventTime = {ticketData.eventTime}
+            />
+          ))}
+          </div>
+        </div>
+        
+        
+    )   
+     
+}
