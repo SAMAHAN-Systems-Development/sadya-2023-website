17b55c91f2f233bb961381f4b703ca1646632841
louispawaon <lpawaon@gmail.com>
2023-10-06 14:15:13 +0800

    refactor: multiple changes

--- src/app/EventCard/page.tsx
+++ src/app/EventCard/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import EventCard from "../../components/ui/EventCard";
+
+import imageUrl from "public/assets/sampleImg.png"
+
+import EventCard from "@/components/ui/EventCard";
 
   export default function Home() {
     return (
       <main>
         <EventCard 
+        imageUrl={imageUrl}
         title={'TICKETED ENTRY'} 
         date={'October 25, 2023'} 
         description={'BTOB & Creative Series Short Film Showcase'} 
--- src/components/ui/EventCard.tsx
+++ src/components/ui/EventCard.tsx
@@ -1,35 +1,27 @@
 import React from 'react';
+import type { StaticImageData } from 'next/image';
+import Image from 'next/image';
 
 type CardProps = {
   date: string;
   description: string;
   floor: string;
+  imageUrl: StaticImageData;
   title: string;
 };
 
-const cardStyle = {
-  width: '450px',
-  height: '300px',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-between',
-};
-
-const containerStyle = {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center', 
-};
 
 const EventCard: React.FC<CardProps> = ({
   title,
   date,
   description,
   floor,
+  imageUrl,
 }) => {
   return (
-    <div style={containerStyle}>
-      <div className="rounded-xl overflow-hidden shadow-lg bg-black" style={cardStyle}>
+    <div className='flex relative rounded-xl overflow-hidden lg:w-[450px]'>
+      <Image src={imageUrl} alt='Event Photo' className='absolute object-cover' fill/>
+      <div className="z-10">
         <div className="px-4 py-4">
           <div className="text-base mb-2">{title}</div>
         </div>
