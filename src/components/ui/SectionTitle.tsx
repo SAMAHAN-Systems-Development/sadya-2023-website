88ea4feaf3e311554affa6dde2e791f8735f1b7c
Migs <miguelemmanueldailisan@gmail.com>
2023-10-06 14:00:48 +0800

    refactor: fixed code

--- src/components/ui/SectionTitle.tsx
+++ src/components/ui/SectionTitle.tsx
@@ -9,11 +9,11 @@ const SectionTitle: React.FC<TextProps> = ({ subtitle, title }) => {
   return (
     <>
       <div className="flex flex-col justify-center items-center text-center">
-        <h1 className="flex justify-center text-center font-semibold text-crimson flex-col font-londrina sm:text-8xl text-2xl uppercase">
+        <h1 className="font-semibold text-crimson font-londrina text-2xl sm:text-8xl uppercase">
           {title}
         </h1>
         <div className="w-[31.188rem]">
-          <p className="sm:text-base text-xs font-normal font-inter text-black normal-case justify-center mt-2">
+          <p className="text-xs sm:text-base font-normal font-inter text-black normal-case mt-2 text-center">
             {subtitle}
           </p>
         </div>
