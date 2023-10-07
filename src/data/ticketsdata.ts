0f94d866cb882064fce8a35ce15104b02d268f39
Miraii133 <jiyovalmoria@gmail.com>
2023-10-08 02:05:41 +0800

    feat: add button anchor link to jotforms. (#16)

--- src/data/ticketsdata.ts
+++ src/data/ticketsdata.ts
@@ -13,52 +13,45 @@ export const secondCard_header = {
   
 export const firstCardData: TicketsCardType[] = [
   {
-  eventId: "Event1-F",
+  eventId: "event1_4f_slots",
   eventFloor: "4F",
   eventPrice: 350,
   eventFoodAvailability: "With Food",
   eventBuilding: "Martin Hall",
-  eventSlotLeft: 100,
+  eventSlotLeft: 1000,
   eventTime: "5:00-9:00PM",
+  eventURL: "https://form.jotform.com/232793628503461/prefill/65218df66433388445e7a49a8121",
   },
   {
-  eventId: "Event1-5F",
+  eventId: "event1_5f_slots",
   eventFloor: "5F",
   eventPrice: 300,
   eventFoodAvailability: "Without Food",
   eventBuilding: "Martin Hall",
   eventSlotLeft: 100,
-  eventTime: "5:00-9:00PM"
+  eventTime: "5:00-9:00PM",
+  eventURL: 'https://form.jotform.com/232793628503461/prefill/65218e2e363039d7fd6a5d640e8c'
   },
   {
-  eventId: "Event1-6F",
+  eventId: "event1_6f_slots",
   eventFloor: "6F",
-  eventPrice: 275,
-  eventFoodAvailability: "Without Food",
-  eventBuilding: "Martin Hall",
-  eventSlotLeft: 100,
-  eventTime: "5:00-9:00PM"
-  },
-  {
-  eventId: "Event1-7F",
-  eventFloor: "7F",
   eventPrice: 250,
   eventFoodAvailability: "Without Food",
   eventBuilding: "Martin Hall",
-  eventSlotLeft: 100,
+  eventSlotLeft: 200,
   eventTime: "5:00-9:00PM",
-  }
+  eventURL: 'https://form.jotform.com/232793628503461/prefill/65218e3f363039d88fa82267554c'
+  },
 ]
   
-// useState
 export const secondCardData: TicketsCardType[] = [
   {
-  eventId: "Event2",
+  eventId: "event2_slots",
   eventFloor: "4F",
-  eventPrice: 250,
-  eventFoodAvailability: "With Food",
+  eventPrice: 50,
+  eventFoodAvailability: "Without Food",
   eventBuilding: "Bapa Benny Tudtud Auditorium",
-  eventSlotLeft: 100,
+  eventSlotLeft: 300,
   eventTime: "6:00 - 9:00 PM",
   },
 ]
