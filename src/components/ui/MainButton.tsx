410b8a65a3d6cd15f260f7ac2255cad47df86051
unknown <paololuisramirez@gmail.com>
2023-10-07 21:18:25 +0800

    feat: added changes from pr MainButton #6

--- src/components/ui/MainButton.tsx
+++ src/components/ui/MainButton.tsx
@@ -10,11 +10,11 @@ type MainButtonProps = {
 
 const buttonVariants = cva(
     [
-      'inline-flex items-center gap-[0.625rem] px-[2.1875rem] py-[0.9375rem] overflow-hidden justify-center relative cursor-pointer shadow-lg ',
-      'font-inter w-fit mt-[-0.0625rem] tracking-[0] font-[1.25rem] font-bold leading-[normal] whitespace-nowrap relative xs:text-[16px] sm:text-[16px] md:text-[20px] lg:text-[20px] xl:text-[20px] 2xl:[20px] ',
+      'inline-flex items-center gap-[0.625rem] px-[2.1875rem] py-[0.9375rem] sm:px-[1.75rem] sm:py-[0.75rem] xsm:px-[1.75rem] xsm:py-[0.75rem] overflow-hidden justify-center relative cursor-pointer shadow-lg ',
+      'font-inter w-fit mt-[-0.0625rem] tracking-[0] font-bold leading-[normal] whitespace-nowrap relative text-[1.25rem] xsm:text-[1rem] sm:text-[1rem] md:text-[1.25rem] lg:text-[1.25rem] xl:text-[1.25rem] 2xl:[1.25rem]',
     ],
     {
-      variants: {
+      variants: { 
         type: {
           'get-tickets': 'bg-lavander text-white hover:bg-lightlavander rounded-[625rem]',
           'about': 'px-[3.1875rem] py-[0.9375rem ] outline outline-2 outline-[#333333] text-[#333333] rounded-[0.725rem] hover:text-[#757575] hover:outline-[#757575]',
