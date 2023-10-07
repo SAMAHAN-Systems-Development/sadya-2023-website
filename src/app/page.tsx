0106e26b9acbc4a27961eb982cc898c3ec190f53
louispawaon <lpawaon@gmail.com>
2023-10-07 19:20:49 +0800

    refactor(Metadata): added description for metadata of main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import HomePage from '@/components/Home/HomePage';
 
 export const metadata: Metadata = {
   title: 'Sadya 2023',
-  description: '',
+  description: 'The official Sadya 2023 Website of the Ateneo de Davao University',
 };
 
 export default function Home() {
