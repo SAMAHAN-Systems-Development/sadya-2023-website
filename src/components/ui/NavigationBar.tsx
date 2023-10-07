c25b9d1d193831d13d5dcae93da7a943d36652ef
unknown <paololuisramirez@gmail.com>
2023-10-07 20:10:10 +0800

    feat: added branding logo and responsiveness NavigationBar #4

--- src/components/ui/NavigationBar.tsx
+++ src/components/ui/NavigationBar.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 
+import SadyaLogo from "public/assets/images/SADYA LOGOTYPE 3D 1.png";
+
 import MainButton from "@/components/ui/MainButton";
 
 const NavigationBar = () => {
   return (
     <div className="flex items-start justify-between px-6 py-4 bg-gradient-to-b from-white via-white-50 to-transparent h-[11.25rem] fold:h-[11.25rem] xs:h-[11.25rem] sm:h-[11.25rem] md:h-[11.25rem] lg:h-[11.25rem] xl:h-[11.25rem] 2xl:h-[11.25rem]">
-      <div className="relative font-extrabold text-black text-[1.875rem] tracking-0 leading-normal fold:text-[1.25rem] xs:text-[1.875rem] sm:text-[1.875rem] md:text-[1.875rem] lg:text-[1.875rem] xl:text-[1.875rem] 2xl:text-[1.875rem]">
-        <div>SADYA</div>
-        <div>2023</div>
+      <div className="relative font-extrabold text-black text-[1.875rem] tracking-0 leading-normal xs:text-[1.875rem] sm:text-[1.875rem] md:text-[1.875rem] lg:text-[1.875rem] xl:text-[1.875rem] 2xl:text-[1.875rem]">
+        <img className="relative w-[123px] h-[80px] sm:w-[98.4px] sm:h-[64px]" alt="Sadya LOGOTYPE" src={SadyaLogo.src} />
       </div>
       <div>
         <MainButton label="GET TICKETS" type="get-tickets" />
-        <button />
+        {/* Add other navigation items/buttons as needed */}
       </div>
     </div>
   );
