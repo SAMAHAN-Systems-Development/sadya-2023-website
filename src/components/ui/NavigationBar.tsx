c56805befe8847cb04670e213c454df962f24039
unknown <paololuisramirez@gmail.com>
2023-10-07 21:39:32 +0800

    feat: fixed responsiveness, changed img asset NavigationBar #4

--- src/components/ui/NavigationBar.tsx
+++ src/components/ui/NavigationBar.tsx
@@ -8,7 +8,7 @@ const NavigationBar = () => {
   return (
     <div className="flex items-start justify-between px-6 py-4 bg-gradient-to-b from-white via-white-50 to-transparent h-[11.25rem] fold:h-[11.25rem] xs:h-[11.25rem] sm:h-[11.25rem] md:h-[11.25rem] lg:h-[11.25rem] xl:h-[11.25rem] 2xl:h-[11.25rem]">
       <div className="relative font-extrabold text-black text-[1.875rem] tracking-0 leading-normal xs:text-[1.875rem] sm:text-[1.875rem] md:text-[1.875rem] lg:text-[1.875rem] xl:text-[1.875rem] 2xl:text-[1.875rem]">
-        <img className="relative w-[123px] h-[80px] sm:w-[98.4px] sm:h-[64px]" alt="Sadya LOGOTYPE" src={SadyaLogo.src} />
+        <img className="relative w-[98.4px] h-[64px] sm:w-[123px] sm:h-[80px] " alt="Sadya LOGOTYPE" src={SadyaLogo.src} />
       </div>
       <div>
         <MainButton label="GET TICKETS" type="get-tickets" />
