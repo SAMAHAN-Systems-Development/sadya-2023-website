86ad90772aac3b7a2531c2c2d3f6bee5c8e47518
Sunny <maryrosedvillacampa@gmail.com>
2023-10-07 14:07:02 +0800

    chore: remove main, edit data type, and change event card name import (#10)

--- lib/types/EventDataTypes.ts
+++ lib/types/EventDataTypes.ts
@@ -1,13 +1,11 @@
 import type { StaticImageData } from 'next/image';
 
 export type EventDataTypes = {
-  imageUrl: any;
-  // imageUrl: StaticImageData;
   eventDataDate: string;
   eventDataDescription: string;
   eventDataEntry: string;
   eventDataFloor: string;
   expiry: string;
   start: string;
-  imageUrl?: StaticImageData;
+  imageUrl: StaticImageData;
 };
--- src/components/CurrentlyHappeningSection/CurrentlyHappeningSection.tsx
+++ src/components/CurrentlyHappeningSection/CurrentlyHappeningSection.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 
-import EventCard from "@/components/CurrentlyHappeningSection/MapEventCard";
+import EventCards from "@/components/CurrentlyHappeningSection/MapEventCard";
 import SectionUI from "../SectionUI";
 import SectionTitle from "../ui/SectionTitle";
 SectionTitle
 
 export default function Home() {
     return (
-        <main>
             <SectionUI>
             <SectionTitle 
             subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
             title="currently happening"/>
-            <EventCard />
+            <EventCards />
             </SectionUI>
-        </main>
     );
 }
