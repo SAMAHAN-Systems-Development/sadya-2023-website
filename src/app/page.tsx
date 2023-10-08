5b25f91a8e6d68574deb8de5fb1b393e5ae0bedc
Migs <miguelemmanueldailisan@gmail.com>
2023-10-08 18:03:55 +0800

    refactor: added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import CurrentlyHappeningSection from '@/components/Sections/CurrentlyHappening/
 import TicketSection from '@/components/Sections/TicketSection/TicketSection';
 import NavigationBar from '@/components/ui/NavigationBar';
 import ActivitiesSection from '@/components/Sections/ActivitiesSection/ActivitiesSection';
+import HomePage from '@/components/Home/HomePage';
 
 export const metadata: Metadata = {
   title: 'SADYA 2023: The Ateneo U-Fest',
@@ -16,6 +17,7 @@ export default function Home() {
   return (
     <div className="bg-white bg-gradient-to-t from-lightlavander relative">
       <NavigationBar />
+      <HomePage />
       <CurrentlyHappeningSection />
       <ActivitiesSection />
       <TicketSection />
--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,18 +1,12 @@
-import Button from '@/components/ui/Button';
-import MainButton from '@/components/ui/MainButton';
+import SectionUI from '@/components/SectionUI';
+import Hero from '@/components/ui/Hero';
 
 const HomePage = () => {
   return (
     <>
-      <div>
-        <Button label="Primary Button" type="primary" />
-        <Button label="Secondary Button" type="secondary" />
-        <Button label="Large Button" size="large" />
-        <MainButton label="GET TICKETS" type="get-tickets" />
-      </div>
-      <div className="text-6xl uppercase">
-        <h1 className="font-londrina">Londrina</h1>
-      </div>
+      <SectionUI>
+        <Hero />
+      </SectionUI>
     </>
   );
 };
